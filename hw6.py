198296a0d59638615e8c5e4c1359f31096ee86e6
Nathan Swetzof <nswetzof@gmail.com>
2024-05-13 22:59:26 -0700

    Begin test functions

--- hw6.py
+++ hw6.py
@@ -3,59 +3,48 @@ Nathan Swetzof
 5/##/24
 """
 
-import string
+import sys, string, pytest
 
-class Graph:
-    def __init__(self, file_name):
-        self.digraph = False
-        self.matrix = []
-        
-        with open(file_name, 'r') as file:
-            for line in file.readlines():
-                self.matrix.append(line.strip('\n').split('\t'))
-                
-            # check if matrix represents an undirected graph
-            i = 0
-            while not self.digraph and i < len(self.matrix):
-                j = 0
-                while not self.digraph and j < i:
-                    if self.matrix[i][j] != self.matrix[j][i]:
-                        self.digraph = True
-                        self.edge_symbol = '->'
-                    
-                    j += 1
-                i += 1
+def createMatrix(file_name):
+    matrix = []
     
-    def isDigraph(self):
-        return self.digraph
+    with open(file_name, 'r') as file:
+            for line in file.readlines():
+                matrix.append(line.strip('\n').split('\t'))
+                
+    return matrix
     
 
 class GraphBuilder:
     LETTER_COUNT = 26
     
-    def __init__(self, graph):
-        self.graph = graph
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.digraph = False
         
-        if graph.isDigraph():
-            self.graph_type = 'digraph'
-            self.edge_symbol = '->'
-        else:
-            self.graph_type = 'graph'
-            self.edge_symbol = '--'
+        # check if matrix represents an undirected graph
+        i = 0
+        while not self.digraph and i < len(self.matrix):
+            j = 0
+            while not self.digraph and j < i:
+                if self.matrix[i][j] != self.matrix[j][i]:
+                    self.digraph = True
+                    self.edge_symbol = '->'
+                
+                j += 1
+            i += 1
             
         self.nodes = []
         
-        for i in range(len(graph.matrix)):
+        for i in range(len(self.matrix)):
             self.nodes.append(str(i + 1))
-        
-    # def createMatrix(self, file_name):
-    #     with open(file_name, 'r') as file:
-    #         for line in file.readlines():
-    #             self.matrix.append(line.strip('\n').split('\t'))
             
     def generateGraph(self, file_name):
         with open(file_name, 'w') as file:
-            file.write(f'{self.graph_type} {{\n')
+            if self.digraph:
+                file.write('digraph {\n')
+            else:
+                file.write('graph {\n')
             file.write(self.generateNodes(1))
             file.write(self.generateEdges(1))
             file.write('}')
@@ -69,23 +58,28 @@ class GraphBuilder:
         return output
             
     def generateEdges(self, current_indent = 0):
-        m = self.graph.matrix
         letters = string.ascii_lowercase # TODO: use this, probably move up to affect self.nodes
         output = ''
-        max_col = len(m)
+        max_col = len(self.matrix)
         
-        for i in range(len(m)):
-            if not self.graph.isDigraph():
+        for i in range(len(self.matrix)):
+            if not self.digraph:
                 max_col = i
             for j in range(max_col):
-                if m[i][j] != '0':
+                if self.matrix[i][j] != '0':
                     output += '\t' * current_indent + \
                         f'{self.nodes[i]} {self.edge_symbol} {self.nodes[j]}\n'
                     
         return output
+    
+    def __str__(self):
+        return self.generateNodes() + self.generateEdges()
 
 if __name__ == "__main__":
-    g = Graph('adj4.txt')
-    gb = GraphBuilder(g)
-    print(gb.generateEdges())
-    gb.generateGraph("output.dot")
+    # gb = GraphBuilder('adj4.txt')
+    # print(gb.generateEdges())
+    # gb.generateGraph("output.dot")
+    
+    # invoke testing code via pytest
+    # "-rA" argument included to list all tests, including passed tests
+    sys.exit(pytest.main(["-rA"]))
--- hw6_test.py
+++ hw6_test.py
@@ -4,23 +4,56 @@ Nathan Swetzof
 """
 
 import pytest
+from hw6 import GraphBuilder
 
-@pytest.fixture
-def tempFile(file_name, contents):
-    with open(file_name, 'w') as file:
-            file.write("")
-            
-    return file_name
+# @pytest.fixture
+# def create_temp_file(tmpdir, name):
+#     sub = tmpdir.mkdir('sub')
+#     file = sub.join()
 
 class TestGraphBuilder():
-    empty_graph = ""
+    empty_graph = []
+    single_node = [1]
+    disjoint_graph = [[0, 0],
+                      [0, 0]]
+    one_edge_digraph = [[0, 1],
+                        [0, 0]]
+    one_edge_graph = [[0, 1],
+                         [1, 0]]
+    two_edge_graph = [[0, 1],
+                      [1, 0]]
     
-    
-    @pytest.fixture
-    def empty_graph():
-        file_name = 'empty_graph.txt'
-        with open(file_name, 'w') as file:
-            file.write("")
+    # @pytest.fixture
+    # def empty_graph(self, tmpdir):
+    #     file_name = 'empty.txt'
+    #     with open(file_name, 'w') as file:
+    #         file.write('')
             
-        return file_name
+    #     return file_name
+    
+    # @pytest.fixture
+    # def single_node(self):
+    #     file_name = 'single_node.txt'
+    #     with open(file_name, 'w') as file:
+    #         file.write('0')
+            
+    #     return file_name
+    
+    def test_EmptyGraph(self):
+        graph = GraphBuilder(self.empty_graph)
+        
+        assert graph.generateNodes() == ''
+        assert graph.generateEdges() == ''
+        
+    def testSingleNode(self, capfd):
+        graph = GraphBuilder(self.single_node)
+        
+        assert graph.generateNodes() == '1\n'
+        assert graph.generateEdges() == ''
+        
+    # def create_temp_file(self, tmpdir, file_name):
+    #     p = tmpdir.join(file_name)
+        
+    #     yield p.path
+    
         
